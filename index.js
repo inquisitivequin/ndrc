a7ca4ac81b99ac4d05db996e43bff1c1a49a286a
Jerd <inquisitivequin@gmail.com>
2017-10-28 16:28:22 -0700

    oAuth

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const passport = require('passport')
 const GoogStrat = require('passport-google-oatuh20').Strategy
 const app = express()
 
+//!!!!DO NOT PUSH THIS TO GITHUB!!!!!
+//client ID 9914596456-6su3e2mh73n9ar4pp47080r9gk0i8rhl.apps.googleusercontent.com
+//client seceret 1BNjFpH2UGwbt0Ad-czBbfcy
 passport.use(new GoogStrat())
 
 
