6747bf4509920804eb5be85a86ed1b31fbcf6aaf
Jerd <inquisitivequin@gmail.com>
2017-11-16 16:19:04 -0800

    yerp

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
 const express = require('express')
 const passport = require('passport')
 //GoogStrat, that's short for GoogleStrategy yay
-const GoogStrat = require('passport-google-oatuh20').Strategy
+const GoogStrat = require('passport-google-oauth20').Strategy
 const app = express()
 const keys = require('./config/keys')
 
 
-passport.use(new GoogleStrat({
-    clientID: GOOGLE_CLIENT_ID,
-    clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://www.example.com/auth/google/callback"
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    User.findOrCreate({ googleId: profile.id }, function (err, user) {
-      return cb(err, user);
-    });
-  }
-));
+passport.use(
+	new GoogStrat({
+	    clientID: keys.googleClientID,
+	    clientSecret: keys.googleClientSecret,
+	    callbackURL: 'auth/google/callback'
+	  },
+	    accessToken => {
+	    	console.log(accessToken)
+	  })
+	);
 
-app.post('/login',
-  passport.authenticate('local'),
-  function(req, res) {
-    // If this function gets called, authentication was successful.
-    // `req.user` contains the authenticated user.
-    res.redirect('/users/' + req.user.username);
-  });
+
+app.get(
+	'/auth/google', 
+	passport.authenticate('google',
+		{ scope: ['profile', 'email']
+	})
+);
+
+app.get('/auth/auth/google/callback',
+	passport.authenticate('google'))
 
 
 const PORT = process.env.PORT || 4200
